f10733ac218cf27592a9152b39606ee56e7fa990
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2021-09-23 12:29:27 +1200

    Fix linking content

--- routes/form.js
+++ routes/form.js
@@ -1,12 +1,9 @@
 const router = require("express").Router()
-//const path = require('path');
 let form_controller = require('../controller/form.js')
 
-//console.log(typeof path)
 router
-  //  .set('assets', path.join(__dirname, 'assets'))
     .get('/', form_controller.index)
-    .get('/', form_controller.about)
+    .get('/about', form_controller.about)
     .get('/project', form_controller.project)
 
 module.exports = router
