8b3d3647cf7c2618acfa42e823a12d19236ef69d
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2021-09-29 11:34:51 +1300

    Make navbar sticky

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,4 @@
 window.addEventListener('load' , (event) => {
-    document.querySelectorAll(".topNav").forEach(el =>{
-        el.addEventListener('mouseenter', (event) => {
-            event.target.style.background ="red";
-        });
-    });
-    document.querySelectorAll(".topNav").forEach(el =>{
-        el.addEventListener('mouseleave', (event) => {
-            event.target.style.background ="rgb(34, 228, 253)";
-        });
-    });
     document.querySelectorAll(".activate").forEach(el =>{
         el.addEventListener('click' , (e) => {
             e.target.nextElementSibling.classList.toggle("show")
