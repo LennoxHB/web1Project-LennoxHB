704829fb3dad417a72e923edd84fc8d5478ef8b1
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2021-10-18 16:29:29 +1300

    Add scroll event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 window.addEventListener('load' , (event) => {
+
     document.querySelectorAll(".activate").forEach(el =>{
         el.addEventListener('click' , (e) => {
             e.target.nextElementSibling.classList.toggle("show")
         });
+    });
+
+    let prevPos = 0
+    window.addEventListener('scroll', (e) =>{
+        pos = window.scrollY
+        if (pos > prevPos){
+            console.log("scrolldown")
+        }else{
+            console.log("scrollup")
+        }
+        prevPos = pos
     });
 });
