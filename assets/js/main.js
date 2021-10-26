044c2ad1c3b5a01201c290d0d38b4f8fc4b5addc
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2021-10-27 11:59:24 +1300

    Better replicate inspiration site bottom nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,17 @@ window.addEventListener('load' , (event) => {
         });
     });
 
+    document.querySelectorAll(".botNav div").forEach(el =>{
+            el.addEventListener('mouseover', (e) => {
+                e.target.nextElementSibling.classList.toggle("showBar")
+        });
+    });
+    document.querySelectorAll(".botNav div").forEach(el =>{
+        el.addEventListener('mouseout', (e) => {
+            e.target.nextElementSibling.classList.toggle("showBar")
+    });
+});
+
     let prevPos = 0
     window.addEventListener('scroll', (e) =>{
         pos = window.scrollY
