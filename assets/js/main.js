ddef8d63a39d2829f80de25e5ab218d0b14fcd00
Lennox Blades (1000013880) <BLADLH1@student.op.ac.nz>
2021-11-15 16:50:43 +1300

    Add image that follows curser

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,25 +6,31 @@ window.addEventListener('load' , (event) => {
         });
     });
 
-    // document.querySelectorAll(".botNav div").forEach(el =>{
-    //         el.addEventListener('mouseover', (e) => {
-    //             e.target.nextElementSibling.classList.toggle("showBar")
-    //     });
-    // });
-    // document.querySelectorAll(".botNav div").forEach(el =>{
-    //     el.addEventListener('mouseout', (e) => {
-    //         e.target.nextElementSibling.classList.toggle("showBar")
-    //      });
-    //    });
+    //add an image that follows the curser while the element is hovered.
+    document.querySelectorAll(".expertsList").forEach(el =>{
+        el.addEventListener('mouseover' , (e) =>{
+            let img = document.createElement('img');
+            img.src = "img/sillhouette.jpg"
+            img.className = "expertPortrait"
+            el.append(img);
+            el.addEventListener('mousemove' , (m) =>{
+                img.style.left = m.pageX + 'px'
+                img.style.top = m.pageY + 'px'
+            })
+            el.addEventListener('mouseout' , (e) =>{
+                img.remove()  
+            })
+        })
+    })
 
-    let prevPos = 0
-    window.addEventListener('scroll', (e) =>{
-        pos = window.scrollY
-        if (pos > prevPos){
-            console.log("scrolldown")
-        }else{
-            console.log("scrollup")
-        }
-        prevPos = pos
-    });
+    // let prevPos = 0
+    // window.addEventListener('scroll', (e) =>{
+    //     pos = window.scrollY
+    //     if (pos > prevPos){
+    //         console.log("scrolldown")
+    //     }else{
+    //         console.log("scrollup")
+    //     }
+    //     prevPos = pos
+    // });
 });
