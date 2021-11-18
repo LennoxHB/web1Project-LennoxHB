fac00a937f1404e34f8bf53d839318d516a00d16
LennoxHB <bladlh1@student.op.ac.nz>
2021-11-18 16:55:19 +1300

    Add example API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 window.addEventListener('load' , (event) => {
+    
+    let date = new Date();
 
+    //Expands an element when clicked on
     document.querySelectorAll(".activate").forEach(el =>{
         el.addEventListener('click' , (e) => {
             e.target.nextElementSibling.classList.toggle("show")
         });
     });
 
+    //Couldn't think of a suitable API for the site, so heres a random one.
+    //Gets historical data for holidays
+    (async () =>{
+        let res = await fetch("https://api.getfestivo.com/v2/holidays?country=NZ&year=2020&api_key=972458e42cb140131e377e39fcf17512")
+        let data = await res.json()
+        console.log(data.holidays[0].name)
+    })()
     //add an image that follows the curser while the element is hovered.
     document.querySelectorAll(".expertsList").forEach(el =>{
         el.addEventListener('mouseover' , (e) =>{
